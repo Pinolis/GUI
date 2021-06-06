47968eceacc550171d9f8b4a3f1464c774c4f9fb
BboyCaligola <84084942+BboyCaligola@users.noreply.github.com>
2021-06-06 16:44:22 +0200

    Update disegnoprova.java

--- disegnoprova.java
+++ disegnoprova.java
@@ -3,9 +3,13 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.Canvas;
 import java.awt.event.*;
-public class disegnoprova extends Canvas {
+public class Frame extends Canvas {
+	
 	private String[] Dinastie;
-	public disegnoprova(String[] dinastie) {
+	
+	
+	public Frame(String[] dinastie) 
+	{
 		JFrame finestra= new JFrame();
 		JTextField testo= new JTextField("");
 		testo.setBackground(Color.red);
@@ -13,44 +17,49 @@ public class disegnoprova extends Canvas {
 		testo.setHorizontalAlignment(JTextField.CENTER);
 		Font font = new Font("SansSerif", Font.BOLD, 20);
 		testo.setFont(font);
+		
 		JLabel label = new JLabel("Seleziona la dinastia:                   >");
 		JButton creators= new JButton("creators");
 		//creators.setPreferredSize(new Dimension(70,20)); 
 		label.setForeground(Color.white);
+		
 		finestra.setTitle("SCRAPER project");
-		finestra.setSize(600,400);
+		finestra.setSize(800,800);
 		finestra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		finestra.setResizable(true);
+		
 		JPanel pannello= new JPanel();
 		pannello.add(label);
+		
 		JComboBox lista= new JComboBox(dinastie);
 		lista.setPreferredSize(new Dimension(300,40));
 		lista.setMaximumRowCount(6);
 		lista.setForeground(Color.red);
+		
 		Color color= new Color(220,0,0);
+		
+		finestra.add(new CanvasCreator(new Imperatore("ciao", "ciao")), BorderLayout.CENTER);
 		pannello.setBackground(color);
 		pannello.add(lista,BorderLayout.EAST);
 		finestra.add(pannello,BorderLayout.NORTH);
-		finestra.setVisible(true);
 		finestra.add(testo,BorderLayout.SOUTH);
 		finestra.add(creators, BorderLayout.WEST);
-		lista.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event) {
+		finestra.setVisible(true);
+		
+		lista.addActionListener(new ActionListener()
+		{
+			
+			public void actionPerformed(ActionEvent event) 
+			{
 				JComboBox lista = (JComboBox) event.getSource();
 				Object selected = lista.getSelectedItem();
 				String selected1= String.valueOf(selected);
 				testo.setText(selected1);
 			}
 		});
-	
 	}
-	
-	
-
 	public static void main(String[] args) {
-		String[] dinastie={"","dinastia2","dinastia3","dinastia4","dinastia5","dinastia6","dinastia7","dinastia8"};
-		disegnoprova f= new disegnoprova(dinastie);
-
+		String[] dinastie={"dinastia1", "dinastia2","dinastia3","dinastia4","dinastia5","dinastia6","dinastia7","dinastia8"};
+		Frame f= new Frame(dinastie);
 	}
-
 }
